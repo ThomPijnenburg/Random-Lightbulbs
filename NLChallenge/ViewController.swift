f313c5b25af67b5a24f4c84493d9e07be96b34e5
ThomPijnenburg <tspijnenburg@gmail.com>
2017-02-01 23:59:34 -0500

    add user input field and label

--- NLChallenge/ViewController.swift
+++ NLChallenge/ViewController.swift
@@ -8,36 +8,39 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
     
     var bulbReservoir: [Lightbulb]?
     var bulbSamples: [Lightbulb]?
     var estimatedNumberColors: Float?
+    var iterations: Int?
+    
+    var bulbBtn = UIButton(frame: CGRect(x: 100, y: 100, width: 100, height: 100))
+    
+    var label = UILabel()
+    
+    var textField = UITextField()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        view.backgroundColor = .blue
         
         self.title = "BulbSampler"
+        view.backgroundColor = .white
         
         bulbReservoir = Lightbulb.populate()
-        var total = 0
-        let iterations = 30
         
-        // Start doing a number of sample routines defined by users input
-        for _ in 0..<iterations {
-            let newSample = sampleAndCalculateUniqueColors()
-            total += newSample
-        }
         
-        calculateNumberOfColors(
+        textField.delegate = self
+        textField.keyboardType = .numberPad
         
-        print(Double(total)/Double(iterations))
-        print(total / iterations)
-        print(total % iterations)
+        setupViews()
         
+        let tapRecognizer = UITapGestureRecognizer()
+        tapRecognizer.addTarget(self, action: #selector(ViewController.didTapView))
+        self.view.addGestureRecognizer(tapRecognizer)
+
     }
 
     override func didReceiveMemoryWarning() {
@@ -45,7 +48,89 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-    func calculateNumberOfColors
+    override func viewDidLayoutSubviews() {
+        configureButton()
+    }
+    
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        let invalidCharacters = CharacterSet(charactersIn: "0123456789").inverted
+        return string.rangeOfCharacter(from: invalidCharacters, options: [], range: string.startIndex ..< string.endIndex) == nil
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        iterations = Int(textField.text!)
+    }
+    
+    func didTapView(){
+        self.view.endEditing(true)
+    }
+    
+    func setupViews() {
+        textField.adjustsFontSizeToFitWidth = true
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        textField.backgroundColor = .green
+        
+        label.text = "labelelel"
+        label.textAlignment = .center
+        label.translatesAutoresizingMaskIntoConstraints = false
+        
+        bulbBtn.translatesAutoresizingMaskIntoConstraints = false
+
+        view.addSubview(label)
+        view.addSubview(bulbBtn)
+        view.addSubview(textField)
+        
+        let viewsDictionary = ["bulbBtn": bulbBtn, "label": label, "textvw": textField] as [String : Any]
+        let metricsDictionary = ["bbtn": 100]
+        
+        view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|-[bulbBtn]-|", options: NSLayoutFormatOptions(), metrics: metricsDictionary, views: viewsDictionary))
+        view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|-[label]-|", options: NSLayoutFormatOptions(), metrics: metricsDictionary, views: viewsDictionary))
+        view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|-[textvw]-|", options: NSLayoutFormatOptions(), metrics: metricsDictionary, views: viewsDictionary))
+        view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|-50-[textvw(bbtn)]-[bulbBtn(bbtn)]-[label]-|", options: NSLayoutFormatOptions(), metrics: metricsDictionary, views: viewsDictionary))
+        
+        bulbBtn.addTarget(self, action: #selector(pressBulbButton), for: .touchUpInside)
+    }
+    
+    func configureButton()
+    {
+        bulbBtn.setImage(UIImage(named: "bulb")?.withRenderingMode(.alwaysTemplate),  for: UIControlState())
+        bulbBtn.imageEdgeInsets = UIEdgeInsetsMake(15,15,15,15)
+        bulbBtn.imageView?.contentMode = UIViewContentMode.scaleAspectFit
+        bulbBtn.tintColor = .red
+        bulbBtn.layer.cornerRadius = 0.5 * bulbBtn.bounds.size.width
+        bulbBtn.layer.borderColor = UIColor(red:0.0/255.0, green:122.0/255.0, blue:255.0/255.0, alpha:1).cgColor as CGColor
+        bulbBtn.layer.borderWidth = 2.0
+        bulbBtn.clipsToBounds = true
+    }
+    
+    func pressBulbButton() {
+        //dismiss numberpad
+        self.view.endEditing(true)
+        var total = 0
+        var reps = 1
+        
+        if iterations != nil {
+            reps = iterations!
+            print(reps)
+        }
+        // Start doing a number of sample routines defined by users input
+        for _ in 0..<reps {
+            let newSample = sampleAndCalculateUniqueColors()
+            total += newSample
+        }
+        
+        let colorCount = Float(total)/Float(reps)
+        
+        print(colorCount)
+        print(total / reps)
+        print(total % reps)
+        setLabelToColorCount(count: colorCount)
+    }
+    
+    func setLabelToColorCount(count: Float) {
+        label.text = String(count)
+        return
+    }
     
     func sampleAndCalculateUniqueColors() -> Int {
         bulbSamples = takeSamples(bulbList: bulbReservoir!)
